fd36f60a36c33b73a71331afc3b278a37e48db0c
Kevin McPhillips <github@kevinmcphillips.ca>
2017-08-05 14:48:23 -0400

    Use factory for multiple possible sources

--- definition.rb
+++ definition.rb
@@ -6,22 +6,53 @@ class Definition
     @word = word
   end
 
-  def value
-    return @value if defined? @value
-    if raw = shell_to_dict
-      @value = raw.lines.drop(2).map do |l|
-        l.starts_with?("dict.org") ? "" : l
-      end.join
-    else
-      @value = nil
+  delegate :value, :source, to: :factory
+
+  def factory
+    return @factory if defined? @factory
+
+    SOURCES.each do |klass|
+      source = klass.new(@word)
+
+      if source.value
+        @factory = source
+        return @factory
+      end
     end
 
-    @value
+    @factory = nil
   end
 
-  private
+  class Dict
+    def initialize(word)
+      @word = word
+    end
 
-  def shell_to_dict
-    `dict --formatted --nocorrect --database fd-nld-eng "#{ word }"`.presence
+    def value
+      return @value if defined? @value
+      if raw = shell_to_dict
+        @value = raw.lines.drop(2).map do |l|
+          l.starts_with?("dict.org") ? "" : l
+        end.join
+      else
+        @value = nil
+      end
+
+      @value
+    end
+
+    def source
+      "dict fd-nld-eng"
+    end
+
+    private
+
+    def shell_to_dict
+      `dict --formatted --nocorrect --database fd-nld-eng "#{ @word }"`.presence
+    end
   end
+
+  SOURCES = [
+    Dict
+  ].freeze
 end
