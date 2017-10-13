544ca71553d1dd43bf8e3965a6d0f0ba42a411af
Kevin McPhillips <github@kevinmcphillips.ca>
2017-10-12 21:16:02 -0400

    Show all definition lines

--- models/word.rb
+++ models/word.rb
@@ -2,4 +2,14 @@
 class Word < Sequel::Model
   many_to_one :lesson
   one_to_many :definitions
+
+  def definition_lines
+    if definitions.any?
+      definitions.map do |definition|
+        definition.value.split("/n")
+      end.flatten.compact
+    else
+      []
+    end
+  end
 end
